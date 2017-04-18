e9839fee123dcaa1a3037bd25dfc24cd1f88c1f1
letsget23 <letsget23@gmail.com>
2017-04-18 21:56:16 +0900

    Add account model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def current_account
+    @current_account ||= Account.find(session[:login_info]) if session[:login_info]
+  end
+
+
 end
