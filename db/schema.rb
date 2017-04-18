b6ae44de0255c8e86cbd87139439d43f9d1da3b6
letsget23 <letsget23@gmail.com>
2017-04-19 02:05:19 +0900

    Refactor accounts controller code

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170418145308) do
+ActiveRecord::Schema.define(version: 20170418170610) do
 
   create_table "accounts", force: :cascade do |t|
     t.string   "email",              default: "", null: false
     t.string   "encrypted_password", default: "", null: false
     t.datetime "created_at",                      null: false
     t.datetime "updated_at",                      null: false
+    t.boolean  "is_signed_in"
     t.index ["email"], name: "index_accounts_on_email", unique: true
   end
 
