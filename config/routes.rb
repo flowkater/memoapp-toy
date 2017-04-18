42a17707205c4996bcbde75acd34b02872bae945
letsget23 <letsget23@gmail.com>
2017-04-18 23:19:27 +0900

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,13 @@ Rails.application.routes.draw do
   get 'hello_world', to: 'hello_world#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  
+  post 'signup', to: 'accounts#signup'
+  post 'signin', to: 'accounts#signin'
+  get 'getinfo', to: 'accounts#getinfo'
+  delete 'logout', to: 'accounts#logout'
+
+  post 'memos', to: 'memos#create'
+  get 'memos', to: 'memos#index'
+  delete 'memos/:id', to: 'memos#destroy'
+  patch 'memos/:id', to: 'memos#update'
 end
