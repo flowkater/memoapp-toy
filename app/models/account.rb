88189e471bc62c2b692c931f4296f26706a0cf62
letsget23 <letsget23@gmail.com>
2017-04-18 23:18:02 +0900

    Add accout model base

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,3 +1,14 @@
 class Account < ApplicationRecord
   has_secure_password
+
+  has_many :memos
+
+  def self.find_account(username, password)
+    self.find_by(username: username).try(:authenticate, password.to_s)
+  end
+
+  def self.find_account_by_token(token)
+    return nil unless token.present?
+    self.find_by(auth_token: token)
+  end
 end
--- db/migrate/20170418121116_create_accounts.rb
+++ db/migrate/20170418121116_create_accounts.rb
@@ -2,7 +2,8 @@ class CreateAccounts < ActiveRecord::Migration[5.0]
   def change
     create_table :accounts do |t|
       t.string :username
-      t.string :password
+      t.string :password_digest
+      t.string :auth_token
 
       t.timestamps
     end
