0fd965befbfff523746f399c7892ff8909acc752
letsget23 <letsget23@gmail.com>
2017-04-27 15:19:59 +0900

    Change routes sub for API

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,14 @@ Rails.application.routes.draw do
   devise_for :accounts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  post 'signup', to: 'accounts#signup'
-  post 'signin', to: 'accounts#signin'
-  delete 'logout', to: 'accounts#logout'
+  post 'api/signup', to: 'accounts#signup'
+  post 'api/signin', to: 'accounts#signin'
+  delete 'api/logout', to: 'accounts#logout'
 
-  post 'memos', to: 'memos#create'
-  get 'memos', to: 'memos#index'
-  delete 'memos/:id', to: 'memos#destroy'
-  patch 'memos/:id', to: 'memos#update'
+  post 'api/memos', to: 'memos#create'
+  get 'api/memos', to: 'memos#index'
+  delete 'api/memos/:id', to: 'memos#destroy'
+  patch 'api/memos/:id', to: 'memos#update'
 
-  post 'auth_account', to: 'authentication#authenticate_account'
+  # post 'api/auth_account', to: 'authentication#authenticate_account'
 end
